0567db2a6aa4d368a9767baa592f22cb42f97284
wesley <340308@student.mboutrecht.nl>
2025-09-30 12:26:05 +0200

    save commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 import logo from "../components/logo.png";
+import SubLogo from "../components/SubLogo.png";
 
 export default function Navigation() {
   return (
-    <nav className="navigation">
-      <img src={logo} alt="Logo" className="logo" />
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/list">List</Link>
-      </div>
-    </nav>
+    <>
+      <nav className="navigation">
+        <img src={logo} alt="Logo" className="logo" />
+
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/list">List</Link>
+        </div>
+      </nav>
+
+      <nav className="SubNavigation">
+        <img src={SubLogo} alt="SubLogo" className="SubLogo" />
+      </nav>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,33 @@ import React from "react";
 
 const Home = () => (
   <div>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <h1 >Home</h1>
+    <p className="header-sub-text">
+      Set up your own team right from the homepage and start managing your members with ease.
+    </p>
+
+    <div className="Team-Container">
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+      <div>
+        <h1>hi</h1>
+      </div>
+    </div>
+
+
   </div>
 );
 
